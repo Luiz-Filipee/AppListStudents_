08c9dcc23d8f54fc79671dbaadb4db35052dbd12
luizfilipe <luizkato8@Gmail.com>
2024-02-20 10:26:17 -0400

    feat: inserçao da validaçao de email na hora de salvar ou editar

--- app/src/main/java/br/com/alura/agenda/ui/activity/FormularioAlunoActivity.java
+++ app/src/main/java/br/com/alura/agenda/ui/activity/FormularioAlunoActivity.java
@@ -1,13 +1,15 @@
 package br.com.alura.agenda.ui.activity;
 
+import static br.com.alura.agenda.ui.activity.ConstantesActivities.CHAVE_ALUNO;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import br.com.alura.agenda.R;
@@ -21,6 +23,8 @@ public class FormularioAlunoActivity extends AppCompatActivity {
     private EditText campoEmail;
     private Aluno aluno;
     private final AlunoDAO dao = new AlunoDAO();
+    private static final String TITULO_APPBAR_NOVO_ALUNO = "Novo Aluno";
+    private static final String TITULO_APPBAR_Edita_ALUNO = "Edita Aluno";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,35 +33,53 @@ public class FormularioAlunoActivity extends AppCompatActivity {
         inicializacaoDosCampos();
         configuraBotaoSalvar();
         configuraBotaoVoltar();
+        carregaAluno();
+    }
 
+    private void carregaAluno() {
         Intent dados = getIntent(); // Pegando o intent da outra classe
-        if(dados.hasExtra("aluno")){ // verificando se o extra "aluno" existe
-            aluno = (Aluno) dados.getSerializableExtra("aluno");
-            campoNome.setText(aluno.getNome());
-            campoTelefone.setText(aluno.getTelefone());
-            campoEmail.setText(aluno.getEmail());
-        }else{
-           aluno = new Aluno();
+        if (dados.hasExtra(CHAVE_ALUNO)) { // verificando se o extra "aluno" existe
+            setTitle(TITULO_APPBAR_Edita_ALUNO);
+            aluno = (Aluno) dados.getSerializableExtra(CHAVE_ALUNO);
+            preencheCampos();
+        } else {
+            setTitle(TITULO_APPBAR_NOVO_ALUNO);
+            aluno = new Aluno();
         }
     }
 
-    private void configuraBotaoSalvar() {
-       Button btnSalvar = findViewById(R.id.activity_formulario_aluno_botao_salvar);
-       btnSalvar.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               preencheAluno();
-               if(aluno.temIdValido()){
-                   dao.edita(aluno);
-               }else{
-                   dao.salva(aluno);
-               }
-               finish();
-           }
-       });
+    private void preencheCampos() {
+        campoNome.setText(aluno.getNome());
+        campoTelefone.setText(aluno.getTelefone());
+        campoEmail.setText(aluno.getEmail());
     }
 
-    private void configuraBotaoVoltar(){
+    private void configuraBotaoSalvar() {
+        Button btnSalvar = findViewById(R.id.activity_formulario_aluno_botao_salvar);
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finalizaFormulario();
+            }
+        });
+    }
+
+    private void finalizaFormulario() {
+        preencheAluno();
+        if (aluno.temIdValido()) {
+            dao.edita(aluno);
+            finish();
+        } else {
+           if (aluno.isEmailValid(aluno.getEmail()) == false){
+               Toast.makeText(this,"Email invalido. Tente novamente", Toast.LENGTH_SHORT).show();
+           }else{
+               dao.salva(aluno);
+               finish();
+           }
+        }
+    }
+
+    private void configuraBotaoVoltar() {
         ImageView setaVoltar = findViewById(R.id.iv_close_activity);
         setaVoltar.setOnClickListener(new View.OnClickListener() {
             @Override
