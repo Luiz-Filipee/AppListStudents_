ce7d9c9b1243d8ca6237975c55288c4838ef5e74
luizfilipe <luizkato8@Gmail.com>
2024-02-20 10:27:11 -0400

    feat: teste 1 AlunoDAAO

---
+++ app/src/test/java/br/com/alura/agenda/dao/AlunoDAOTest.java
@@ -0,0 +1,29 @@
+package br.com.alura.agenda.dao;
+
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.alura.agenda.model.Aluno;
+
+public class AlunoDAOTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testSave() {
+        Aluno aluno = new Aluno("luizfilipe","luizkato@gmail.com","123124123");
+        AlunoDAO dao = new AlunoDAO();
+        dao.salva(aluno);
+        assertTrue(dao.todos().contains(aluno));
+    }
+
+    public void testGetAllAlunos() {
+        Aluno aluno = new Aluno("luizfilipe","luizkato@gmail.com","123124123");
+        AlunoDAO da = new AlunoDAO();
+        da.salva(aluno);
+        assertFalse(da.todos().isEmpty());
+    }
+}
