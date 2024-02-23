5cddf710324b799f00dcdc20f9846144450beeff
luizfilipe <luizkato8@Gmail.com>
2024-02-23 00:35:55 -0400

    feat: criaçao de menus context para remover um usuario

--- app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
@@ -4,15 +4,22 @@ import static br.com.alura.agenda.ui.activity.ConstantesActivities.CHAVE_ALUNO;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.zip.Inflater;
 
 import br.com.alura.agenda.R;
 import br.com.alura.agenda.dao.AlunoDAO;
@@ -30,7 +37,38 @@ public class ListaAlunosActivity extends AppCompatActivity {
         setTitle(TITULO_APPBAR);
         configuraFabNovoAluno();
         configuraLista();
-        dao.salva(new Aluno("Luiz Filipe","123124123","luizkato8@gmail.com"));
+
+        dao.salva(new Aluno("luiz filipe", "2312312312", "luizkato8@gmail.com"));
+        dao.salva(new Aluno("2", "2312312312", "luizkato8@gmail.com"));
+        dao.salva(new Aluno("3", "2312312312", "luizkato8@gmail.com"));
+        dao.salva(new Aluno("luiz 4", "2312312312", "luizkato8@gmail.com"));
+        dao.salva(new Aluno("luiz 5", "2312312312", "luizkato8@gmail.com"));
+        dao.salva(new Aluno("luiz 6", "2312312312", "luizkato8@gmail.com"));
+        dao.salva(new Aluno("luiz 7", "2312312312", "luizkato8@gmail.com"));
+        dao.salva(new Aluno("luiz 8", "2312312312", "luizkato8@gmail.com"));
+        dao.salva(new Aluno("luiz 9", "2312312312", "luizkato8@gmail.com"));
+        dao.salva(new Aluno("luiz 9", "2312312312", "luizkato8@gmail.com"));
+        dao.salva(new Aluno("luiz e", "2312312312", "luizkato8@gmail.com"));
+        dao.salva(new Aluno("luiz 1", "2312312312", "luizkato8@gmail.com"));
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater()
+                .inflate(R.menu.activity_lista_alunos_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        int itemRemover = item.getItemId();
+        if (itemRemover == R.id.item_remover_alunos) {
+            AdapterView.AdapterContextMenuInfo menuInfo =
+                    (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+            Aluno alunoEscolhido = adapter.getItem(menuInfo.position);
+            removeAluno(alunoEscolhido);
+        }
+        return super.onContextItemSelected(item);
     }
 
     private void configuraFabNovoAluno() {
@@ -63,18 +101,7 @@ public class ListaAlunosActivity extends AppCompatActivity {
         ListView listaDeAlunos = findViewById(R.id.list_view);
         configuraAdapter(listaDeAlunos);
         configuraListenerDeCliquePorItem(listaDeAlunos);
-        configuraListenerDeCliqueLongo(listaDeAlunos);
-    }
-
-    private void configuraListenerDeCliqueLongo(ListView listaDeAlunos){
-        listaDeAlunos.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long id) {
-                Aluno alunoEscolhido = (Aluno) adapterView.getItemAtPosition(position);
-                removeAluno(alunoEscolhido);
-                return true;
-            }
-        });
+        registerForContextMenu(listaDeAlunos);
     }
 
     private void removeAluno(Aluno aluno) {
